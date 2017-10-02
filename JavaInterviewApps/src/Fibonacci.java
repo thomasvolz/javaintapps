96275164d2cd722ff92320c343842072e8fbce4d
thomasvolz <thomasvolz@u.boisestate.edu>
2017-10-02 16:33:15 -0600

    simpleFib function creation

--- JavaInterviewApps/src/Fibonacci.java
+++ JavaInterviewApps/src/Fibonacci.java
@@ -1,4 +1,26 @@
+/**
+ * 
+ * @author Thomas Volz
+ * Yes, I am actually Bartholomew Fibonacci
+ * Inventor of the Fibonacci Code, now a novel 
+ * and soon to be a major motion picture
+ *
+ */
 
 public class Fibonacci {
+	
+	/**
+	 * A basic Fibonacci Series function that
+	 * will return the value from the Fib Series length specified
+	 * @param seriesIndex the series length and value at that length
+	 * @return the calculated result from the position in the Fib Series
+	 */
+	private int simpleFib(int seriesIndex)
+	{
+		int numberOne=0;
+		int numberTwo=1;
+		int result=0;
+		return result;
+	}
 
 }
