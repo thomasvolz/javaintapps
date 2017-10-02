d343e5429297f49eb6563696abcdaf92f7229122
thomasvolz <thomasvolz@u.boisestate.edu>
2017-10-02 16:19:55 -0600

    Initial Repo

---
+++ JavaInterviewApps/src/AppSelector.java
@@ -0,0 +1,21 @@
+/**
+ * AppSelector
+ * @author Thomas Volz
+ * @version 0.1
+ * 
+ * This main method will eventually allow you to select
+ * the particular function you want to see executed.
+ * 
+ * For now, executions are hard coded
+ *
+ */
+
+
+public class AppSelector {
+
+	public static void main(String[] args) {
+		System.out.println("Hey Buddy");
+
+	}
+
+}
