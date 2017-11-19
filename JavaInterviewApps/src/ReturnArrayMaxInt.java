0e2c0b58b8b40e4e93730d0f0a02aafb2962fde1
thomasvolz <thomasvolz@u.boisestate.edu>
2017-11-19 13:49:03 -0700

    Defined function in ReturnArrayMaxInt

--- JavaInterviewApps/src/ReturnArrayMaxInt.java
+++ JavaInterviewApps/src/ReturnArrayMaxInt.java
@@ -7,5 +7,33 @@
  *
  */
 public class ReturnArrayMaxInt {
+	
+	
+	/**
+	 * Thoughts:
+	 * Might be better to operate on an object and return it,
+	 * That way if the initial parameter were null I'd have 
+	 * a cleaner way of detecting / reporting on that
+	 * 
+	 * @param collection
+	 * @return
+	 */
+	public int getMax(int[] collection)
+	{
+		if(collection==null)
+			return 0;
+		
+		int result=0;
+		
+		for(int a=0; a<collection.length; a++)
+		{
+			if(collection[a]>result)
+			{
+				result=collection[a];
+			}
+		}
+		
+		return result;
+	}
 
 }
